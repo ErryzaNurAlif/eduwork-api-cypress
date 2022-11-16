b62021d50e953b772543e9aefae51baa68ca8acd
Erryza Nur Alif <erryzanuralif@gmail.com>
2022-11-16 23:01:08 +0700

    create Auth and Command

---
+++ cypress/e2e/authenticationAPI.cy.js
@@ -0,0 +1,23 @@
+describe("Test Scenario API Authentication", () => {
+  it("Successfully login by appending username and password in URL", () => {
+    cy.visit("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+    cy.get("p").should(
+      "include.text",
+      "Congratulations! You must have the proper crendential"
+    );
+  });
+
+  it("Succesfully login using headers", () => {
+    cy.visit("https://the-internet.herokuapp.com/basic_auth", {
+      headers: {
+        authorization: "Basic YWRtaW46YWRtaW4=",
+      },
+      faiOnStatuscode: false,
+    });
+
+    cy.get("p").should(
+      "include.text",
+      "Congratulations! You must have the proper crendential"
+    );
+  });
+});
