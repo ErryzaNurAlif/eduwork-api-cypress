bb88389d5dced7f278854d98a2c57a2036aaea33
Erryza Nur Alif <erryzanuralif@gmail.com>
2022-11-19 13:57:07 +0700

    validate header

---
+++ cypress/e2e/pokeapi/validateHeaders.cy.js
@@ -0,0 +1,26 @@
+describe("Automation API with Pokemon API", () => {
+  it("Successfully validate content-type", () => {
+    cy.request("https://pokeapi.co/api/v2/pokemon/ditto").as("pokemon");
+    cy.get("@pokemon")
+      .its("headers")
+      .its("content-type")
+      .should("include", "application/json; charset=utf-8");
+
+    // OR
+
+    cy.request({
+      method: "GET",
+      url: "https://pokeapi.co/api/v2/pokemon/ditto",
+      headers: {
+        accept: "application/json; charset=utf-8",
+      },
+    }).as("pokemon");
+
+    cy.get("@pokemon").then((response) => {
+      expect(response.headers).to.have.property(
+        "content-type",
+        "application/json; charset=utf-8"
+      );
+    });
+  });
+});
